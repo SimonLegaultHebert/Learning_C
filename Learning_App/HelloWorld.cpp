12c09ba5508079ff3aff22dce462e5d552d2b41a
SimonLegaultHebert <simon.legault.hebert@gmail.com>
2015-09-16 18:11:25 -0400

    HelloWorld!

---
+++ Learning_App/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Inclut la bibliothèque iostream (affichage de texte)
+
+using namespace std; // Indique quel espace de noms on va utiliser
+
+int main()
+{
+	cout << "Hello world!" << endl; // Affiche un message
+	return 0; // Termine la fonction main et donc le programme
+}
