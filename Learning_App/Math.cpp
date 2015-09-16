4ffb6632069b5ed8002f4e1ea13291af91f7cf91
SimonLegaultHebert <simon.legault.hebert@gmail.com>
2015-09-16 18:47:48 -0400

    add quick Math example

---
+++ Learning_App/Math.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//int main() {
+//	double const nombre = 16;
+//
+//	double resultat = sqrt(nombre);
+//	cout << "La racine de " << nombre << " est " << resultat << endl;
+//
+//	resultat = pow(nombre, 2);
+//	cout << "Le carre de " << nombre << " est " << resultat << endl;
+//
+//	return 0;
+//}
