c0540af39f80bda3bbcf49345fe8c52eb7cba33f
SimonLegaultHebert <simon.legault.hebert@gmail.com>
2015-09-16 18:48:48 -0400

    comment the HelloWorld! example

--- Learning_App/HelloWorld.cpp
+++ Learning_App/HelloWorld.cpp
@@ -2,8 +2,8 @@
 
 using namespace std; // Indique quel espace de noms on va utiliser
 
-int main()
-{
-	cout << "Hello world!" << endl; // Affiche un message
-	return 0; // Termine la fonction main et donc le programme
-}
+//int main()
+//{
+//	cout << "hello world!" << endl; // affiche un message
+//	return 0; // termine la fonction main et donc le programme
+//}
